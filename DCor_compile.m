0b9fd744e64a98dc6250f0312eabe6a2f4a74fcc
OmrySendik <omrysendik@users.noreply.github.com>
2017-08-11 16:04:54 +0300

    Add files via upload

--- DCor_compile.m
+++ DCor_compile.m
@@ -10,4 +10,7 @@ cd matconvnet-1.0-beta20/matlab;
 vl_compilenn;
 cd ..; cd ..;
 
+fprintf('Downloading Pretrained Net (VGG-19). This may take a few minutes...');
+outfilename = websave('./matconvnet-1.0-beta20/PreTrainedNets/imagenet-vgg-verydeep-19.mat','http://www.vlfeat.org/matconvnet/models/imagenet-vgg-verydeep-19.mat');
+
 end
