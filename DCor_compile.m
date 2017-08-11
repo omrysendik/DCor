eb4f372c9a5c94faf7b7a0f555a01750f87da76e
OmrySendik <omrysendik@users.noreply.github.com>
2017-08-11 20:22:28 +0300

    Add files via upload

--- DCor_compile.m
+++ DCor_compile.m
@@ -11,6 +11,7 @@ vl_compilenn;
 cd ..; cd ..;
 
 fprintf('Downloading Pretrained Net (VGG-19). This may take a few minutes...');
-outfilename = websave('./matconvnet-1.0-beta20/PreTrainedNets/imagenet-vgg-verydeep-19.mat','https://www.dropbox.com/s/2q3w7vosvi1jpsu/imagenet-vgg-verydeep-19.mat?dl=0');
+FolderToDl = [cd,'/matconvnet-1.0-beta20/PreTrainedNets/imagenet-vgg-verydeep-19.mat'];
+unix(['wget -O ',FolderToDl,' https://www.dropbox.com/s/ndyqhp4umkpww8t/imagenet-vgg-verydeep-19.mat?dl=0 --no-check-certificate']);
 
 end
